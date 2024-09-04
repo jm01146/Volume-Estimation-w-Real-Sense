cd50f0cecf12e80afcd24d6e807d90c2808b4b91
John Marcial <59844100+jm01146@users.noreply.github.com>
2024-09-04 02:26:06 -0400

    Update RealSense-OpenCV.py

--- RealSense-OpenCV.py
+++ RealSense-OpenCV.py
@@ -1,1 +1,25 @@
-This will be the coding thing here
+import PredictScales import ScalePxDimensions
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+ctx = rs.context()
+rs_device = []
+for reItems in ctx.devices:
+  rs_device.append(rsItems.get_info(rs.camera_info.serial_number))
+
+cam_1 = rs.pipeline()
+config_1 = rs.config()
+config_1.enable_device(rs_device[0])
+config_1.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config_1.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+cam_2 = rs.pipeline()
+config_2 = rs.config()
+config_2.enable_device(rs_device[1])
+config_2.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config_2.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+cam_1.start(config_1)
+cam_2.start(config_2)
+
